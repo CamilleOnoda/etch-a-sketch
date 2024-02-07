bae7dad1c67577e41392409b047c16cd6399481f
CamilleOnoda <info@camilleonoda.com>
2024-02-07 17:08:40 +0900

    Create a function to get the selected color and pass it to another to change th bg color of each square clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createGrid() {
-    let gridSize = 256;
+    let gridSize = 256; //16x16
 
     let grid = document.querySelector('.grid');
 
@@ -7,8 +7,25 @@ function createGrid() {
         let square = document.createElement('div');
         square.classList.add('squares');
         grid.appendChild(square);
-//        create 60px box
     }
-}
+};
 
+function getSelectedColor(e) {
+    let colorPicker = document.querySelector('#colorPicker');
+    let selectedColor = e.currentTarget.value;
+    colorPicker.value = selectedColor;
+    changeBackgroundColor(selectedColor);
+};
+
+function changeBackgroundColor(selectedColor) {
+    let gridSquares = document.querySelectorAll('.squares');
+    gridSquares.forEach(square => {
+        square.addEventListener('click', () => {
+            console.log(selectedColor);
+            square.style.backgroundColor = selectedColor;
+        });
+    });
+};
+
+colorPicker.addEventListener('input', getSelectedColor);
 createGrid();
